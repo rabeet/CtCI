bc149b67f3f0634eb44576a226f5017df54c19d5
Rabeet <raf6000@gmail.com>
2017-09-18 16:29:10 -0400

    Couple CTCI problems

---
+++ src/Prob4_1.java
@@ -0,0 +1,18 @@
+
+public class Prob4_1 {
+
+	
+	/**
+	 * What is a balanced tree:
+	 * 		2
+	 * 	3		 4
+	 * 7  2    1   5
+	 *      8        0
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/Prob4_2.java
@@ -0,0 +1,21 @@
+
+public class Prob4_2 {
+
+	
+	/*
+	 * Given a directed graph determine whether there is a path
+	 * b/w 2 nodes:
+	 * 
+	 * Doesn't have to be shortest path
+	 * Bruteforce way: 
+	 * Recursive call from every node attached.
+	 * If the node is node 2 then return true. Else If the node is end node then false. else recursion.
+	 * 
+	 *  
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}

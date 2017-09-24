33ade8616af84502c278f412ab434554a57aa059
Rabeet <raf6000@gmail.com>
2017-09-24 05:10:51 -0400

    4.2 - lots of helper code

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class Utilities {
 	
@@ -6,6 +10,51 @@ public class Utilities {
 		
 	}
 
+}
+
+class DirectedNode {
+	public int data;
+	public List<DirectedNode> neighbors;
+	public DirectedNode(int data) {
+		this.data = data;
+		neighbors = new ArrayList<DirectedNode>();
+	}
+	
+	public void addNeighbor(DirectedNode d) {
+		if (this.neighbors.contains(d)) return;
+		this.neighbors.add(d);
+	}
+	
+	public String toString() {
+		String s = this.data+"->\n";
+		for (DirectedNode d : neighbors) {
+			s = s+"   "+d.data+"\n";
+		}
+		return s;
+	}
+	
+	public String toString(DirectedNode node) {
+		String s = node.data+"->\n";
+		for (DirectedNode d : node.neighbors) {
+			s = s+"   "+d.data+"\n";
+		}
+		return s;
+	}
+	
+	private String getTree(DirectedNode node, Set<DirectedNode> set) {
+		String ans = toString(node);
+		set.add(node);
+		for ( DirectedNode d : node.neighbors) {
+			if (set.add(d))
+				ans += getTree(d, set);
+			else continue;
+		}
+		return ans;
+	}
+	
+	public String getTree() {
+		return getTree(this,new HashSet<DirectedNode>());
+	}
 }
 
 class TreeNode {

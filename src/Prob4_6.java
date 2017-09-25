a9b477990abf7e462b0e381831f91ce35fcf3c34
Rabeet <raf6000@gmail.com>
2017-09-25 00:22:07 -0400

    prob 4.7

--- src/Prob4_6.java
+++ src/Prob4_6.java
@@ -9,10 +9,21 @@ public class Prob4_6 {
 	private static TreeNode found = null;
 	
 	public static void main(String[] args) {
-		int [] arr = {1,2,3,4,5};
+		int [] arr = {1,2,3,4,5,6,7,8,9};
 		TreeNode node = TreeNode.serializeTree(arr);
 		firstCommonAncestor(node, node.left, node.left.left);
+		TreeNode n2 = firstCommonAncestor2(node, node.right.left, node.right.right);
+		System.out.println(n2.data);
 		System.out.println(found.data);
+	}
+	
+	public static TreeNode firstCommonAncestor2(TreeNode root, TreeNode a, TreeNode b) {
+		if (root == null || a == null || b == null) return null;
+		if (find(root.left, a) && find(root.left,b))
+			return firstCommonAncestor2(root.left, a, b);
+		if (find(root.right, a) && find(root.right, b))
+			return firstCommonAncestor2(root.right, a, b);
+		else return root;
 	}
 
 	public static void firstCommonAncestor(TreeNode root, TreeNode a, TreeNode b) {
@@ -28,7 +39,6 @@ public class Prob4_6 {
 
 	private static boolean find(TreeNode root, TreeNode a) {
 		if (root == null) return false;
-		if (a == null) return false;
 		return (root == a)? true: find(root.left, a) || find(root.right, a);
 	}
 
